89a61b200dae38e094d7373e9e932cce8b284ce8
ncc02 <cuongtk2002@gmail.com>
2024-10-29 19:56:31 +0700

    update setting.py db

--- knowledgeGraph/settings.py
+++ knowledgeGraph/settings.py
@@ -85,15 +85,12 @@ WSGI_APPLICATION = "knowledgeGraph.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
